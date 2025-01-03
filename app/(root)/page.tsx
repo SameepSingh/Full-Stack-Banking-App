3e1742cadd7907653ebb4cd4a36384dfce6ec944
SameepSingh <sameep@uoguelph.ca>
2025-01-03 12:26:10 -0500

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox';
 import React from 'react';
 
-const Home: React.FC = () => {
+const Home = () => {
+    const loggedIn = {firstName: 'Sameep' };
+
+
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <section className="home">
+            <div className="home-conent">
+                <header className="home-header">
+                    <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently" 
+
+                    />
+
+                </header>
+            </div>
+            
+        </section>
     );
 };
 
 export default Home;
+//properrties of home
